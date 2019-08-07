0ec8a8d0e5ad05ff591b0b28779408b23d76d0b2
Edhotmetal <edward@flashnetsolutions.com>
2019-08-07 13:21:29 -0400

    The game is functional now
    Fixed the logic and cleaned up the code so the game works now.

--- 21_cards.rb
+++ 21_cards.rb
@@ -9,23 +9,33 @@ require_relative 'deck.rb'
 require 'Qt4'
 
 class CardField < Qt::Widget
-	slots 'columnOnePressed()', 'columnTwoPressed()', 'columnThreePressed()'
+	slots 'columnOnePressed()', 'columnTwoPressed()', 'columnThreePressed()', 'startGame()'
 
 	def initialize()
 		super
 
-		@round = 0 # The number of times the player has selected a column
+		@round = 1 # The number of times the player has selected a column
 		@deck = Deck.new
 		@main_hand = Hand.new
 		@column_one = Hand.new
 		@column_two = Hand.new
 		@column_three = Hand.new
 		@reveal = false # True when the program knows the player's card
+		@playing = false # True when a game is playing
 		@player_card = nil
-		@message = "Select a card and point to the column that it is in!"
+		@message = "Three columns of cards will be displayed before the player.\n Select a card then point to the column that contains it!\n Click Deal to start playing."
 		@image = Qt::Image.new("cards.png")
-		first_deal
-		deal_cards
+	end
+
+	def startGame()
+		if(@playing) then
+			@message = "Select the column that your card is in!"
+		else
+			@playing = true
+			first_deal
+			deal_cards
+			@round = 1
+		end
 	end
 
 	# Take 21 cards from the deck and place them into the main hand
@@ -36,25 +46,32 @@ class CardField < Qt::Widget
 			@main_hand.add_card(@deck.deal_card)
 		end
 		puts("main_hand contains #{@main_hand.get_card_count} cards")
+		@column_one.clear
+		@column_two.clear
+		@column_three.clear
 	end
 
 	# Deal the 21 cards in the main hand to the three columns
 	def deal_cards()
 		puts("Laying out the cards")
 		while(@main_hand.get_card_count > 0) do
-			@column_one.add_card(@main_hand.take_card)
-			@column_two.add_card(@main_hand.take_card)
-			@column_three.add_card(@main_hand.take_card)
+			@column_one.add_card(@main_hand.take_last_card)
+			@column_two.add_card(@main_hand.take_last_card)
+			@column_three.add_card(@main_hand.take_last_card)
 		end
 		puts("column_one contains #{@column_one.get_card_count} cards")
 		puts("column_two contains #{@column_two.get_card_count} cards")
 		puts("column_three contains #{@column_three.get_card_count} cards")
+		@message = "Choose the column that contains your card!"
 		update
 	end
 
 	# The player has selected column one
 	def columnOnePressed()
-		if(@round < 3) then
+		if(not(@playing)) then
+			@message = "Click Deal to start playing!"
+			update
+		elsif(@round < 3) then
 			puts("Player selected column 1")
 			pick_up_cards(1)
 		else
@@ -64,7 +81,10 @@ class CardField < Qt::Widget
 
 	# The player has selected column two
 	def columnTwoPressed()
-		if(@round < 3) then
+		if(not(@playing)) then
+			@message = "Click Deal to start playing!"
+			update
+		elsif(@round < 3) then
 			puts("Player selected column 2")
 			pick_up_cards(2)
 		else
@@ -74,7 +94,10 @@ class CardField < Qt::Widget
 
 	# The player has selected column three
 	def columnThreePressed()
-		if(@round < 3) then
+		if(not(@playing)) then
+			@message = "Click Deal to start playing!"
+			update	
+		elsif(@round < 3) then
 			puts("Player selected column 3")
 			pick_up_cards(3)
 		else
@@ -109,15 +132,15 @@ class CardField < Qt::Widget
 		case column
 		when 1
 			while(@column_one.get_card_count > 0) do
-				@main_hand.add_card(@column_one.take_card)
+				@main_hand.add_card(@column_one.remove_card_at(0))
 			end
 		when 2
 			while(@column_two.get_card_count > 0) do
-				@main_hand.add_card(@column_two.take_card)
+				@main_hand.add_card(@column_two.remove_card_at(0))
 			end
 		when 3
 			while(@column_three.get_card_count > 0) do
-				@main_hand.add_card(@column_three.take_card)
+				@main_hand.add_card(@column_three.remove_card_at(0))
 			end
 		end
 	end
@@ -133,15 +156,20 @@ class CardField < Qt::Widget
 			@player_card = @column_three.get_card(3)
 		end
 		puts("The player's card is #{@player_card}")
-		@message = "You card is #{@player_card}!"
+		@message = "Your card is #{@player_card}!"
 		update
-		@round = 0
+		@playing = false
 		@player_card = nil
 	end
 
 	def paintEvent(event)
 		painter = Qt::Painter.new(self)
-		draw_cards(painter)
+		if(@playing) then
+			draw_cards(painter)
+		else
+			puts("Game is not playing so displaying message")
+			painter.drawText(Qt::Rect.new(0,50,500,500), Qt::AlignCenter, @message)
+		end
 		painter.end
 	end
 
@@ -150,7 +178,7 @@ class CardField < Qt::Widget
 		# Draw the first column
 		target = Qt::Rect.new # The area where the card will be drawn
 		source = Qt::Rect.new # the area of cards.png that contains the card to be drawn
-		target_x = 50
+		target_x = 40
 		target_y = 0
 		for i in 0..6 do
 			source_x = (@column_one.get_card(i).value - 1) * 79
@@ -162,7 +190,7 @@ class CardField < Qt::Widget
 		end
 
 		# Draw the second column
-		target_x += 120
+		target_x += 150
 		target_y = 0
 		for i in 0..6 do
 			source_x = (@column_two.get_card(i).value - 1) * 79
@@ -174,7 +202,7 @@ class CardField < Qt::Widget
 		end
 
 		# Draw the third column
-		target_x += 120 
+		target_x += 150 
 		target_y = 0
 		for i in 0..6
 			source_x = (@column_three.get_card(i).value - 1) * 79
@@ -186,6 +214,7 @@ class CardField < Qt::Widget
 		end
 
 		if(@player_card != nil) then # If the program knows the player's card, display it
+			puts("displaying the player's card")
 			source_x = (@player_card.value - 1) * 79
 			source_y = @player_card.suit * 123
 			target_x = 130
@@ -195,7 +224,7 @@ class CardField < Qt::Widget
 			painter.drawImage(target, @image, source)
 		end
 
-		painter.drawText(Qt::Rect.new(0,100,300,100), Qt::AlignCenter, tr(@message))
+		painter.drawText(Qt::Rect.new(100,400,250,100), Qt::AlignCenter, tr(@message))
 	end
 end
 
@@ -203,19 +232,23 @@ class CardTrickWidget < Qt::Widget
 	def initialize(parent = nil)
 		super
 
-		one = Qt::PushButton.new(tr("one"))
-		two = Qt::PushButton.new(tr("two"))
-		three = Qt::PushButton.new(tr("three"))
+		one = Qt::PushButton.new(tr("One"))
+		two = Qt::PushButton.new(tr("Two"))
+		three = Qt::PushButton.new(tr("Three"))
 		font = Qt::Font.new("Times", 18, Qt::Font::Bold)
 		one.setFont(font)
 		two.setFont(font)
 		three.setFont(font)
+
+		deal = Qt::PushButton.new(tr("Deal"))
+		deal.setFont(font)
 
 		card_field = CardField.new
 
 		connect(one, SIGNAL("clicked()"), card_field, SLOT("columnOnePressed()"))
 		connect(two, SIGNAL("clicked()"), card_field, SLOT("columnTwoPressed()"))
 		connect(three, SIGNAL("clicked()"), card_field, SLOT("columnThreePressed()"))
+		connect(deal, SIGNAL("clicked()"), card_field, SLOT("startGame()"))
 
 		buttonLayout = Qt::HBoxLayout.new
 		buttonLayout.addWidget(one)
@@ -225,8 +258,9 @@ class CardTrickWidget < Qt::Widget
 		cardLayout = Qt::VBoxLayout.new
 		cardLayout.addWidget(card_field)
 		cardLayout.addLayout(buttonLayout)
+		cardLayout.addWidget(deal)
 		setLayout(cardLayout)
-		resize(500,500)
+		resize(500,600)
 	end
 end
 
